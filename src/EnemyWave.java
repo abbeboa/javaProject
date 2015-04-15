82bc3e66475cb36065db3835a2bc5c2ac0bc74e4
chrizze3 <chrisse_th@hotmail.com>
2015-04-15 18:58:09 +0200

    minor change
    nothing big edited

--- src/EnemyWave.java
+++ src/EnemyWave.java
@@ -2,4 +2,13 @@ public class EnemyWave {
     private long waveStartTime = System.currentTimeMillis();
     private int spawnEnemiesInterval = 6000;
     private int numberOfWaves;
+    private int waveNumber;
+
+    public EnemyWave(int waveNumber) {
+        this.waveNumber = waveNumber;
+        setVariables(waveNumber);
+    }
+    private void setVariables(int waveNumber) {
+
+    }
 }
